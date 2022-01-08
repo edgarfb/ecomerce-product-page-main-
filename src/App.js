3ebf28f7b322dd58522fecbdbb5695a4d148fd80
Edgar Britez <edgarfbritez@gmail.com>
2022-01-08 10:42:56 -0300

    Sneakers Description skeleton

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
+import SneakersDescription from "./components/SneakersDescription";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Slider></Slider>
+      <SneakersDescription />
     </div>
   );
 }
