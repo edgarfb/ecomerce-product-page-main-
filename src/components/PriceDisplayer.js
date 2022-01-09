2cd9016df4ee097e0b06871930aab113ec85a595
Edgar Britez <edgarfbritez@gmail.com>
2022-01-09 19:44:10 -0300

    finish the styles

--- src/components/PriceDisplayer.js
+++ src/components/PriceDisplayer.js
@@ -1,21 +1,45 @@
 import React from "react";
 import styled from "styled-components";
+import { COLORS } from "../styles";
 
 const PriceContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  margin: 2rem 0;
 `;
 
-const Price = styled.div``;
-const PriceWithDiscount = styled.div``;
-const Discount = styled.div``;
+const Price = styled.div`
+  color: ${COLORS.grayBlue};
+  text-decoration: line-through;
+  font-weight: 700;
+`;
+const PriceWithDiscount = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+const Discount = styled.div`
+  background-color: ${COLORS.lightGrayBlue};
+  padding: 0.5rem;
+  color: ${COLORS.orange};
+  border-radius: 0.5rem;
+  margin-left: 1.5rem;
+  font-size: 1rem;
+  font-weight: 700;
+`;
 
+// TODO: maybe the price should be getting from props?
 const PriceDisplayer = () => {
   return (
     <PriceContainer>
-      <PriceWithDiscount>125</PriceWithDiscount>
-      <Discount>50%</Discount>
-      <Price>250</Price>
+      <PriceWithDiscount>
+        $125.00
+        <Discount>50%</Discount>
+      </PriceWithDiscount>
+
+      <Price>$250.00</Price>
     </PriceContainer>
   );
 };
