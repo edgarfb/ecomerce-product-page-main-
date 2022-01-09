5837a2f951e1436612a02909e71e2fea61aa70ab
Edgar Britez <edgarfbritez@gmail.com>
2022-01-09 12:55:30 -0300

    styled main content text

--- src/components/SneakersDescription.js
+++ src/components/SneakersDescription.js
@@ -1,26 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import { COLORS } from "../styles";
 
 const Container = styled.div`
   position: relative;
+  padding: 1.5rem;
 `;
 
-const Name = styled.h2``;
+const Name = styled.h2`
+  color: ${COLORS.orange};
+  text-transform: uppercase;
+  font-size: 0.8rem;
+`;
 
 const Title = styled.h1``;
 
-const Description = styled.p``;
+const Description = styled.p`
+  color: ${COLORS.gray};
+  line-height: 1.5;
+  font-size: 0.9rem;
+`;
 
 const SneakersDescription = ({ name, title, description }) => {
   return (
     <Container>
       <Name>The companie´s name</Name>
-      <Title>Fall Limited Editio Sneakers</Title>
+      <Title>Fall Limited Edition Sneakers</Title>
       <Description>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-        voluptatem officia quisquam deleniti sequi magni ratione! Doloremque, et
-        odit corrupti velit eos voluptas minus a iure itaque ipsa, sapiente
-        explicabo?
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they’ll withstand everything the
+        weather can offer.
       </Description>
     </Container>
   );
