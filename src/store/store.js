ca2b84f4287681fda1aa2c410f70117c3777eb15
Edgar Britez <edgarfbritez@gmail.com>
2022-01-09 09:33:11 -0300

    create a reducer for menu --- mobile-menu

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers } from "redux";
 import { sliderReducer } from "./slider";
 import { amountReducer } from "./amount";
 import { cartReducer } from "./cart";
+import { menuReducer } from "./menu";
 
 export const store = createStore(
   combineReducers({
     slider: sliderReducer,
     amount: amountReducer,
     cart: cartReducer,
+    menu: menuReducer,
   })
 );
