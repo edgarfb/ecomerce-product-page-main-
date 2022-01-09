3069e8d9a107c12f9fde5af272507d07215629d8
Edgar Britez <edgarfbritez@gmail.com>
2022-01-09 12:54:46 -0300

    add styles.js file

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  black: "#000",
+  white: "#fff",
+  veryDarkblue: "hsl(220, 13%, 13%)",
+  gray: "hsl(219, 9%, 45%)",
+  grayBlue: "hsl(220, 14%, 75%)",
+  lightGrayBlue: "hsl(223, 64%, 98%)",
+  orange: "hsl(26, 100%, 55%)",
+  orangePale: "hsl(25, 100%, 94%)",
+};
